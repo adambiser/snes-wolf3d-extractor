f0acf5023db56e8011a6872f230514a69ec9f311
Adam Biser <adambiser@gmail.com>
2017-07-23 02:26:27 -0400

    Use new MainApplication Tk class.

--- extractor.py
+++ extractor.py
@@ -2,10 +2,11 @@ import extractor.ui.mainapplication as ui
 import Tkinter as tk
 
 def main():
-    root = tk.Tk()
-    root.title('SNES Wolfenstein 3D Extractor')
-    root.minsize(400, 100)
-    ui.MainApplication(root).pack(side="top", fill="both", expand=True)
-    root.mainloop()
+##    root = tk.Tk()
+##    root.title('SNES Wolfenstein 3D Extractor')
+##    root.minsize(400, 100)
+##    ui.MainApplication(root).pack(side="top", fill="both", expand=True)
+##    root.mainloop()
+    ui.MainApplication().mainloop()
 
 main()
