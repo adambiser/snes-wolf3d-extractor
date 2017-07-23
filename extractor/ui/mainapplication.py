8115cb468d858eca2e838afd917a5c5edd4095a3
Adam Biser <adambiser@gmail.com>
2017-07-23 03:23:53 -0400

    Center window.

--- extractor/ui/mainapplication.py
+++ extractor/ui/mainapplication.py
@@ -15,10 +15,11 @@ class MainApplication(tk.Tk):
         self.options_frame = OptionsFrame(self, self.settings)
         # Perform layout.
         pad = {'padx':5, 'pady':5}
-        self.minsize(400, 100)
         self.rom_frame.pack(anchor=tk.NW, fill=tk.X, **pad)
         self.options_frame.pack(fill=tk.X, **pad)
 ##        self.pad_children(self, 5)
+        self.minsize(500, 200)
+        self.center_window(500, 200)
 
     def on_closing(self):
         self.settings.save()
@@ -28,3 +29,12 @@ class MainApplication(tk.Tk):
 ##        for widget in parent.winfo_children():
 ##            widget.pack(padx=pad, pady=pad)
 ##            self.pad_children(widget, pad)
+
+    def center_window(self, width, height):
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
+
+        # calculate position x and y coordinates
+        x = (screenwidth - width) / 2
+        y = (screenheight - height) / 2
+        self.geometry('%dx%d+%d+%d' % (width, height, x, y))
