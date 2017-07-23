2553f42fbc4c36008382afc859fe45ac9bf628a5
Adam Biser <adambiser@gmail.com>
2017-07-23 01:02:31 -0400

    Now saves to and from a file by converting to dict.

--- extractor/ui/settings.py
+++ extractor/ui/settings.py
@@ -3,12 +3,10 @@ import json
 import os
 import Tkinter as tk
 if __name__ == "__main__":
-    # Sloppy hack to get the relative import to work without using -m flag.
+    # Sloppy hack to get the relative import to work when running this as main without using -m flag.
     import sys
     sys.path.append('../..')
-    from extractor.entrytype import *
-else:
-    from ..entrytype import *
+from extractor.entrytype import *
 
 class Settings():
     # TODO Use AppData path for config file?
@@ -26,25 +24,33 @@ class Settings():
 
     def save(self):
         with open(Settings.CONFIG_FILE, 'w') as f:
-            json.dump(self.settings, f, indent=4, separators=(',', ': '))
+            json.dump(self.to_dict(), f, indent=4, separators=(',', ': '))
 
     def to_dict(self):
-        #if not key.startswith('__') and not callable(key)
-        return {key:value.get() if isinstance(value, tk.IntVar) else value for key, value in self.__dict__.items()}
+        settings = { 'rom_file': self.rom_file }
+        settings['export'] = {key:value.get() if isinstance(value, tk.IntVar) else value for key, value in self.export.items()}
+        return settings
 
     def from_dict(self, settings):
         self.rom_file = settings.get('rom_file', '')
-        self.export_sprites = tk.IntVar(value=settings.get('export_sprites', 1))
+        export = settings.get('export', {})
+        self.export = dict([(key,tk.IntVar(value=export.get(key, 1))) for key in Settings.get_export_types()])
+
+    @staticmethod
+    def get_export_types():
+        cls = list({name for name, cls in globals().items() if inspect.isclass(cls) and cls != AbstractEntry and issubclass(cls, AbstractEntry)})
+        cls.sort()
+        return cls
 
 # For testing.
 if __name__ == "__main__":
     root = tk.Tk()
+    print "get_export_types: "
+    print Settings.get_export_types()
+    print
     settings = Settings()
-    settings.from_dict({'rom_file':'folder/file.sfc', 'export_sprites': 0})
+    settings.from_dict({'rom_file':'folder/file.sfc', 'export': {'Sprite': 0}})
+    print "json encode: "
     print json.JSONEncoder(indent=4, separators=(',', ': ')).encode(settings.to_dict())
-    for cls in dir(): #[dir() if not cls.startswith('__')]:
-        cls = globals()[cls]
-        if inspect.isclass(cls) and (cls != AbstractEntry) and issubclass(cls, AbstractEntry):
-            print cls.__name__
-##    print settings.rom_file
-##    print settings.export_sprites.get()
+    print ""
+    print settings.to_dict()
