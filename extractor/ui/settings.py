3b83aed3aa1057589328d4bf9c93f35233db2b19
Adam Biser <adambiser@gmail.com>
2017-07-24 19:52:01 -0400

    Added export_to_subfolder and combine_maps
    Also, output_folder now called export_folder.
    Added rom_file and export_folder validation. If they no longer exist,
    they are reset back to defaults.

--- extractor/ui/settings.py
+++ extractor/ui/settings.py
@@ -14,7 +14,9 @@ class Settings():
 
     def __init__(self):
         self.rom_file = tk.StringVar()
-        self.output_folder = tk.StringVar(value=os.getcwd())
+        self.export_folder = tk.StringVar(value=os.getcwd())
+        self.export_to_subfolder = tk.BooleanVar(value=True)
+        self.combine_maps = tk.BooleanVar(value=True)
         self.export = dict([(key,tk.IntVar(value=1)) for key in Settings.get_export_types()])
         self.load()
 
@@ -30,7 +32,9 @@ class Settings():
     def to_dict(self):
         settings = {
             'rom_file': self.rom_file.get(),
-            'output_folder': self.output_folder.get(),
+            'export_folder': self.export_folder.get(),
+            'export_to_subfolder': self.export_to_subfolder.get(),
+            'combine_maps': self.combine_maps.get(),
             }
         settings['export'] = {key:value.get() for key, value in self.export.items()}
         return settings
@@ -38,7 +42,13 @@ class Settings():
     def from_dict(self, settings):
         # Be sure to use .set so that the only variable value updates and the variable reference doesn't change.
         self.rom_file.set(settings.get('rom_file', ''))
-        self.output_folder.set(settings.get('output_folder', os.getcwd()))
+        if not os.path.isfile(self.rom_file.get()):
+            self.rom_file.set('')
+        self.export_folder.set(settings.get('export_folder', os.getcwd()))
+        if not os.path.isdir(self.export_folder.get()):
+            self.export_folder.set(os.getcwd())
+        self.export_to_subfolder.set(settings.get('export_to_subfolder', True))
+        self.combine_maps.set(settings.get('combine_maps', True))
         export = settings.get('export', {})
         for key in Settings.get_export_types():
             self.export[key].set(export.get(key, 1))
