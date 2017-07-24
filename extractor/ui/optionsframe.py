24973fe5301c7956c6f5abc8601715e5d421efbb
Adam Biser <adambiser@gmail.com>
2017-07-24 19:53:42 -0400

    Added checkboxes for new settings.
    "export to subfolder" and "combine maps".

--- extractor/ui/optionsframe.py
+++ extractor/ui/optionsframe.py
@@ -4,25 +4,39 @@ class OptionsFrame(tk.LabelFrame):
     COLUMNS = 3
     def __init__(self, parent, settings, **options):
         tk.LabelFrame.__init__(self, parent, **options)
-        self.config(text='Export Settings')
+        self.config(text='Export Options',
+                    padx=5,
+                    pady=5)
         self.parent = parent
-        self.settings = settings
         # Add widgets.
+        tk.Checkbutton(self,
+                       text='Export to subfolder named after ROM',
+                       variable=settings.export_to_subfolder,
+                       ).pack(anchor=tk.NW,
+                              )
+        tk.Checkbutton(self,
+                       text='Combine maps into single file',
+                       variable=settings.combine_maps,
+                       ).pack(anchor=tk.NW,
+                              )
+        subframe = tk.LabelFrame(self, text='ROM Entry Types')
+        subframe.pack(fill=tk.X,
+                      )
         keys = settings.export.keys()
         keys.sort()
         row = 0
         col = 0
         for key in keys:
-            tk.Checkbutton(self,
+            tk.Checkbutton(subframe,
                            text=key,
-                           variable=self.settings.export[key]
+                           variable=settings.export[key],
                            ).grid(row=row,
                                   column=col,
-                                  sticky=tk.W
+                                  sticky=tk.W,
                                   )
             col += 1
             if col == OptionsFrame.COLUMNS:
                 row += 1
                 col = 0
         for col in range(OptionsFrame.COLUMNS):
-            tk.Grid.columnconfigure(self, col, weight=1)
+            tk.Grid.columnconfigure(subframe, col, weight=1)
