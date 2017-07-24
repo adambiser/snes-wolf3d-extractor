c628d4efb66d788a2b9cec2220ddfbd775b85b4f
Adam Biser <adambiser@gmail.com>
2017-07-24 19:52:46 -0400

    Finalized StatusText class.
    Used to display a status log for the program.

--- extractor/ui/statustext.py
+++ extractor/ui/statustext.py
@@ -1,23 +1,27 @@
 import Tkinter as tk
 
 class StatusText(tk.LabelFrame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.LabelFrame.__init__(self, parent, *args, **kwargs)
+    def __init__(self, parent, **options):
+        tk.LabelFrame.__init__(self, parent, **options)
         self.config(text='Status')
+        self.parent = parent
         scrollbar = tk.Scrollbar(self,
                                  orient=tk.VERTICAL,
                                  )
-        self.text = tk.Text(self,
-                            state=tk.DISABLED,
-                            yscrollcommand=scrollbar.set,
-                            )
-        scrollbar.config(command=self.text.yview)
-        scrollbar.pack(side=tk.RIGHT,
+        self.textbox = tk.Text(self,
+                               state=tk.DISABLED,
+                               height=1,
+                               yscrollcommand=scrollbar.set,
+                               )
+        scrollbar.config(command=self.textbox.yview)
+        scrollbar.pack(anchor=tk.NE,
+                       side=tk.RIGHT,
                        fill=tk.Y,
                        padx=(0,5),
                        pady=5,
                        )
-        self.text.pack(side=tk.LEFT,
+        self.textbox.pack(anchor=tk.NW,
+                       side=tk.LEFT,
                        fill=tk.BOTH,
                        expand=1,
                        padx=(5,0),
@@ -25,12 +29,9 @@ class StatusText(tk.LabelFrame):
                        )
 
     def appendline(self, chars):
-        if self.text.get('1.0', 'end-1c'):
+        if self.textbox.get('1.0', 'end-1c'):
             chars = '\n' + chars
-        self.insert(tk.END, chars)
-        
-    def insert(self, index, chars, *args):
-        self.text.config(state=tk.NORMAL)
-        self.text.insert(index, chars, *args)
-        self.text.config(state=tk.DISABLED)
-        self.text.see(tk.END)
+        self.textbox.config(state=tk.NORMAL)
+        self.textbox.insert(tk.END, chars)
+        self.textbox.config(state=tk.DISABLED)
+        self.textbox.see(tk.END)
