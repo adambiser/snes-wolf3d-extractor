3319c9a8636f0fd06347f3f76fe2721e57ab940b
Adam Biser <adambiser@gmail.com>
2017-07-24 21:02:09 -0400

    Cleaned code.

--- extractor.py
+++ extractor.py
@@ -1,11 +1,6 @@
 import extractor.ui.mainapplication as ui
-import Tkinter as tk
 
 def main():
-##    root = tk.Tk()
-##    root.title('SNES Wolfenstein 3D Extractor')
-##    root.minsize(400, 100)
-##    ui.MainApplication(root).pack(side="top", fill="both", expand=True)
     root = ui.MainApplication()
     root.mainloop()
 
